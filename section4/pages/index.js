e1299a600fd4908bf49072f67685ff2a1ddd6d1a
MoSangBin <mosangbin94@gmail.com>
2022-11-22 21:18:18 +0900

    Section4: route project

--- section4/pages/index.js
+++ section4/pages/index.js
@@ -1,9 +1,12 @@
-import styles from "../styles/Home.module.css";
+import { getFeaturedEvents } from "../dummy-data";
+import EventList from "../components/events/event-list";
 
 export default function HomePage() {
+  const featuredEvents = getFeaturedEvents();
+
   return (
     <div>
-      <h1>The Home Page</h1>
+      <EventList items={featuredEvents} />
     </div>
   );
 }
